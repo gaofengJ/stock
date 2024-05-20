b557c0815c8537d899a9591b803261d9995b51a5
木风 <>
2024-05-20 20:28:45 +0800

    feat: update front eslint

--- apps/front/.eslintrc.js
+++ apps/front/.eslintrc.js
@@ -1,3 +1,17 @@
 module.exports = {
+  extends: ['airbnb', 'airbnb/hooks', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  rules: { // 在这里可以添加或覆盖规则
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }], // 控制哪些文件扩展名可以包含 JSX 语法
+  },
   // extends: 'next/core-web-vitals',
 };
