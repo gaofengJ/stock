a4543fc33d2146fd157a93c7a49ea7fcd084e248
gaofengJ <760613910@qq.com>
2024-06-01 17:57:22 +0800

    feat: add swagger

--- apps/back/src/main.ts
+++ apps/back/src/main.ts
@@ -1,9 +1,34 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import type { INestApplication } from '@nestjs/common';
 import { AppModule } from './app.module';
 
+/**
+ * 初始化 Swagger 文档
+ */
+const initSwagger = (app: INestApplication) => {
+  const options = new DocumentBuilder()
+    .setTitle('Stock Back')
+    .setDescription('Stock Back API')
+    .setVersion('1.0')
+    .setBasePath('api')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/docs', app, document);
+};
+
+/**
+ * 初始化应用
+ */
 const bootstrap = async () => {
-  const app = await NestFactory.create(AppModule);
+  const appOptions = { cors: true };
+  const app = await NestFactory.create(AppModule, appOptions);
   app.setGlobalPrefix('api');
+
+  initSwagger(app);
+
   await app.listen(3000);
 };
+
 bootstrap();
