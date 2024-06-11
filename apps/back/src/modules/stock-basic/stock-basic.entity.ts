d75532aa41c35e768a6085a2ca0ccf5441b2b94f
木风 <>
2024-06-11 17:49:58 +0800

    feat: update env

--- apps/back/src/modules/stock-basic/stock-basic.entity.ts
+++ apps/back/src/modules/stock-basic/stock-basic.entity.ts
@@ -3,7 +3,10 @@ import { Column, Entity } from 'typeorm';
 import { CommonEntity } from '@/entity/common.entity';
 
 // 使用 @Entity 装饰器将这个类标记为一个实体，并指定数据库表名为 't_stock_basic'
-@Entity('t_stock_basic')
+@Entity({
+  name: 't_stock_basic',
+  comment: '股票基本信息',
+})
 // 定义一个名为 StockBasicEntity 的类，并继承 CommonEntity
 export class StockBasicEntity extends CommonEntity {
   @Column({
