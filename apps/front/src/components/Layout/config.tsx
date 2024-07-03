6808a2dcd2b42e1b6419dd050aec1bfe8c650818
木风 <>
2024-07-03 20:45:27 +0800

    feat: update config

--- apps/front/src/components/Layout/config.tsx
+++ apps/front/src/components/Layout/config.tsx
@@ -47,7 +47,7 @@ export const avatarDropdownItems: MenuProps['items'] = [
   },
   {
     key: EAvatarDropdownKey.SWITCH_ACCOUNT,
-    label: <span>切换账户</span>,
+    label: <span>切换账号</span>,
   },
 ];
 
@@ -63,53 +63,35 @@ export interface IrouteItem {
 }
 
 // 路由对应信息
-export const routeItems: IrouteItem[] = [
+export const analysisSiderMenuItems: MenuProps['items'] = [
   {
-    key: 'home',
-    showAside: false,
+    key: '/data/quota',
+    // icon: React.createElement(LineChartOutlined),
+    label: <Link href="/data/quota">情绪指标</Link>,
   },
   {
-    key: 'market',
-    showAside: false,
+    key: '/data/limit',
+    // icon: React.createElement(RiseOutlined),
+    label: <Link href="/data/limit">连板统计</Link>,
   },
   {
-    key: 'data',
-    showAside: true,
-    siderMenuItems: [
+    key: '/data/limit-review',
+    // icon: React.createElement(BarsOutlined),
+    label: <Link href="/data/limit-review">涨停板复盘</Link>,
+  },
+  {
+    key: '/data/basic',
+    // icon: React.createElement(TableOutlined),
+    label: <Link href="/data/basic">基础信息</Link>,
+    children: [
       {
-        key: '/data/quota',
-        // icon: React.createElement(LineChartOutlined),
-        label: <Link href="/data/quota">情绪指标</Link>,
+        key: '/data/basic/stock',
+        label: <Link href="/data/basic/stock">个股基本信息</Link>,
       },
       {
-        key: '/data/limit',
-        // icon: React.createElement(RiseOutlined),
-        label: <Link href="/data/limit">连板统计</Link>,
-      },
-      {
-        key: '/data/limit-review',
-        // icon: React.createElement(BarsOutlined),
-        label: <Link href="/data/limit-review">涨停板复盘</Link>,
-      },
-      {
-        key: '/data/basic',
-        // icon: React.createElement(TableOutlined),
-        label: <Link href="/data/basic">基础信息</Link>,
-        children: [
-          {
-            key: '/data/basic/stock',
-            label: <Link href="/data/basic/stock">个股基本信息</Link>,
-          },
-          {
-            key: '/data/basic/daily',
-            label: <Link href="/data/basic/daily">每日交易数据</Link>,
-          },
-        ],
+        key: '/data/basic/daily',
+        label: <Link href="/data/basic/daily">每日交易数据</Link>,
       },
     ],
-  },
-  {
-    key: 'review',
-    showAside: false,
   },
 ];
