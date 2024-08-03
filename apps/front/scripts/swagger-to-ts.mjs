dfccc62a731a0063b365b70a0e53ae3c7322f0bd
木风 <jingaofeng@bilibili.com>
2024-08-03 17:32:31 +0800

    feat: add swagger-to-ts

---
+++ apps/front/scripts/swagger-to-ts.mjs
@@ -0,0 +1,12 @@
+import run from 'mufeng-swagger-to-ts';
+
+run({
+  docUrl: 'http://127.0.0.1:3000/api-docs-json',
+  includePaths: [
+    '/api/source/daily/list',
+    '/api/source/daily/{id}',
+    '/api/source/daily/create',
+    '/api/source/daily/bulk-create',
+    '/api/source/daily/clear',
+  ],
+});
