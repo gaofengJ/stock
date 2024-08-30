75e189a4215780f46f3a62a651d0e57bb9c15d80
木风 <jingaofeng@bilibili.com>
2024-08-30 19:49:40 +0800

    feat: add watermark

--- apps/front/src/app/layout.tsx
+++ apps/front/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { AntdRegistry } from '@ant-design/nextjs-registry';
+import { Watermark } from 'antd';
 import './global.css';
 
 const inter = Inter({ subsets: ['latin'] }); // 仅加载 Inter 字体的拉丁字母子集。这有助于减少字体文件的大小，从而提高页面加载速度
@@ -19,7 +20,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AntdRegistry>{children}</AntdRegistry>
+        <Watermark
+          height={40}
+          width={160}
+          content="木风同学的投资小站"
+        >
+          <AntdRegistry>{children}</AntdRegistry>
+        </Watermark>
       </body>
     </html>
   );
