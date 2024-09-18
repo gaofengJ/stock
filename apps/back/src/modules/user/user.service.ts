55bf0e184044a1e38bcd331b0afccb7fdba4c711
木风 <jingaofeng@bilibili.com>
2024-09-18 16:21:10 +0800

    feat: add user-role

--- apps/back/src/modules/user/user.service.ts
+++ apps/back/src/modules/user/user.service.ts
@@ -7,13 +7,23 @@ import { Pagination } from '@/helper/paginate/pagination';
 
 import { UserEntity } from './entities/user.entity';
 
-import { UserDto, UserQueryDto, UserUpdateDto } from './user.dto';
+import {
+  UserDto,
+  UserQueryDto,
+  UserRoleDto,
+  UserRoleQueryDto,
+  UserRoleUpdateDto,
+  UserUpdateDto,
+} from './user.dto';
+import { UserRoleEntity } from './entities/user_role.entity';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(UserEntity)
     private UserRepository: Repository<UserEntity>,
+    @InjectRepository(UserRoleEntity)
+    private UserRoleRepository: Repository<UserRoleEntity>,
   ) {}
 
   async list({
@@ -57,5 +67,48 @@ export class UserService {
 
   async clear() {
     await this.UserRepository.clear();
+  }
+
+  async userRoleList({
+    pageNum,
+    pageSize,
+    userId,
+    roleId,
+  }: UserRoleQueryDto): Promise<Pagination<UserRoleEntity>> {
+    const queryBuilder = this.UserRoleRepository.createQueryBuilder(
+      't_user_role',
+    ).where({
+      ...(userId && { userId }),
+      ...(roleId && { roleId }),
+    });
+    return paginate(queryBuilder, { pageNum, pageSize });
+  }
+
+  async userRoleDetail(id: number): Promise<UserRoleEntity> {
+    const item = await this.UserRoleRepository.findOneBy({ id });
+    if (!item) throw new NotFoundException('未找到该记录');
+    return item;
+  }
+
+  async userRoleCreate(dto: UserRoleDto) {
+    await this.UserRoleRepository.save(dto);
+  }
+
+  async userRoleBulkCreate(dto: UserRoleDto[]) {
+    const ret = await this.UserRoleRepository.save(dto);
+    return ret?.length || 0;
+  }
+
+  async userRoleUpdate(id: number, dto: UserRoleUpdateDto) {
+    await this.UserRoleRepository.update(id, dto);
+  }
+
+  async userRoleDelete(id: number) {
+    const item = await this.userRoleDetail(id);
+    await this.UserRoleRepository.remove(item);
+  }
+
+  async userRoleClear() {
+    await this.UserRoleRepository.clear();
   }
 }
